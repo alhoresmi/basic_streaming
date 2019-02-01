0732733430d37f2f1ebf46e1fb5527982094f9fd
alhoresmi <alhoresmi@gmail.com>
2019-02-02 01:09:08 +0400

    non async/await example based on websocket-client

--- code/websocket_test.py
+++ code/websocket_test.py
@@ -1,6 +1,13 @@
+"""
+non asyncio example
+"""
+
+
 import websocket
 import queue
 import threading
+import json
+import toolz
 
 
 def on_message(ws, msg):
@@ -9,7 +16,23 @@ def on_message(ws, msg):
 
 def qget(q):
     while True:
-        print(q.get())
+        yield q.get()
+
+
+def process_msg(q):
+    return toolz.thread_last(qget(q),
+                      (map, json.loads),
+                      (map, lambda v: v['event']),
+                      (filter, lambda v: str_filter(v['event_name'])))
+
+
+def str_filter(s):
+    return 'machine learning' in s.lower()
+
+
+def show_stream(q):
+    for v in process_msg(q):
+        print(v)
 
 
 def wsrun():
@@ -21,7 +44,7 @@ ws = websocket.WebSocketApp('ws://stream.meetup.com/2/rsvps',
                             on_message=on_message)
 
 t1 = threading.Thread(target=wsrun, args=())
-t2 = threading.Thread(target=qget, args=(qq,))
+t2 = threading.Thread(target=show_stream, args=(qq,))
 
 t1.start()
 t2.start()
